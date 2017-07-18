9079054fdc84ab2ac0f8f5be0e3f77ba7bdcf6c7
Joey Turko <jturko@mail.uoguelph.ca>
2017-07-18 12:39:06 -0400

    stuff

--- plotEdgeFit.C
+++ plotEdgeFit.C
@@ -1,9 +1,17 @@
 
-void plotEdgeFit(int val = 4)
+void plotEdgeFit(int val = 4, bool proton = true)
 {
-    TFile * f = TFile::Open("BC501A_bkgsub.root");
-    TH1F * h1 = (TH1F*)f->Get(Form("ProtonCal%d",val));
-    
+    TFile * f = NULL;
+    TH1F * h1 = NULL;
+
+    if(proton == true) {
+        f = TFile::Open("BC501A_bkgsub.root");
+        h1 = (TH1F*)f->Get(Form("ProtonCal%d",val));
+    }
+    else {
+        f = TFile::Open("BC537_bkgsub.root");
+        h1 = (TH1F*)f->Get(Form("ScionixCal%d",val));
+    }
     h1->SetStats(kFALSE);
     for(int i=0; i<h1->GetNbinsX(); i++) { if(h1->GetBinContent(i) < 0) h1->SetBinContent(i,0); }
 
@@ -17,10 +25,11 @@ void plotEdgeFit(int val = 4)
     //double maxBin = h1->GetMaximumBin();
     //double maxBinCont = h1->GetMaximum();
 
-    double C10 = maxBinCont*0.1;
-    double C90 = maxBinCont*1;
+    double C10 = maxBinCont*0.125;
+    double C90 = maxBinCont*0.875;
 
-    double C50 = (C10 + C90) / 2.;
+    //double C50 = (C10 + C90) / 2.;
+    double C50 = maxBinCont/2.;
     
     int idx = maxBin;
     double L = 0;
@@ -30,27 +39,35 @@ void plotEdgeFit(int val = 4)
     bool found10 = false;
     bool found90 = false;
     while(!found || !found10 || !found90) {
-        idx++;
         if((h1->GetBinContent(idx) < C50) && !found) {
             found = true;
-            L = h1->GetBinCenter(idx);
+            L = h1->GetBinCenter(idx-1);
             std::cout << " found 50% " << std::endl;
         }
         if((h1->GetBinContent(idx) < C10) && !found10) {
             found10 = true;
-            L10 = h1->GetBinCenter(idx);
+            L10 = h1->GetBinCenter(idx-1);
             std::cout << " found 10% " << std::endl;
         }
         if((h1->GetBinContent(idx) < C90) && !found90) {
             found90 = true;
-            L90 = h1->GetBinCenter(idx);
+            L90 = h1->GetBinCenter(idx-1);
             std::cout << " found 90% " << std::endl;
         }
+        idx++;
     }
     
-    L = (L90+L10)/2.; 
+    //L = (L90+L10)/2.; 
 
+    std::cout << "MAX PH = " << maxBinCont << " @ L = " << h1->GetBinCenter(maxBin) << std::endl;
     std::cout << "PH(87.5%) = " << C90 << " @ L = " << L90 << "\t PH(50%) = " << C50 << " @ L = " << L << "\t PH(12.5%) = " << C10 << " @  L = " << L10 << std::endl;
+    
+
+    h1->SetTitle("");
+    h1->GetYaxis()->SetRangeUser(0,h1->GetMaximum()*1.1);
+    h1->GetXaxis()->SetLabelSize(0.032);
+    h1->GetXaxis()->SetNdivisions(505);    
+
     h1->Draw("h");
     
     //TLine * line10 = new TLine(L10,-maxBinCont*0.05,L10,h1->GetBinContent(h1->FindBin(L10)));
@@ -71,9 +88,9 @@ void plotEdgeFit(int val = 4)
     line50->Draw("same");    
     line90->Draw("same");    
 
-    TLatex * text10 = new TLatex(L10*0.98,-maxBinCont*0.1,"L_{10}");
+    TLatex * text10 = new TLatex(L10*0.98,-maxBinCont*0.1,"L_{12.5}");
     TLatex * text50 = new TLatex(  L*0.98,-maxBinCont*0.1,"L");
-    TLatex * text90 = new TLatex(L90*0.98,-maxBinCont*0.1,"L_{100}");
+    TLatex * text90 = new TLatex(L90*0.98,-maxBinCont*0.1,"L_{87.5}");
 
     text10->Draw("same");
     text50->Draw("same");
